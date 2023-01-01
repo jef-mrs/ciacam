d915bfef64cb73754a02c1a53cf472587f493734
jean francois barbato <jeanf.barbato@gmail.com>
2023-01-01 15:38:47 +0100

    add column eta as date to batches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_29_092152) do
+ActiveRecord::Schema.define(version: 2023_01_01_143533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(version: 2022_12_29_092152) do
     t.integer "status"
     t.string "origin"
     t.text "info"
+    t.date "eta"
     t.index ["product_id"], name: "index_batches_on_product_id"
     t.index ["supplier_id"], name: "index_batches_on_supplier_id"
   end
